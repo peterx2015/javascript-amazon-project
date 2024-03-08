2f6787d5b9564e8acf225d4c977523e0b6900ed9
peterx2015 <liyufeng2012@outlook.com>
2024-03-08 14:06:54 +0800

    ‘添加到购物车’按钮逻辑编写

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,11 +1,11 @@
-console.log('hello');
 
 
-let productsHTML ='';
+
+let productsHTML = '';
 
 
 products.forEach((product) => {
-    productsHTML += `
+  productsHTML += `
                 <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -18,7 +18,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10 }.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -50,15 +50,46 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
     `;
 
-    console.log(productsHTML);
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const productId = button.dataset.productId;
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (item.productId === productId) {
+          matchingItem = item;
+        }
+      })
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+
+
+
+      console.log(cart);
+    })
+  })
+
+const testDiv = document.querySelector('#js-test-div');
+console.log(testDiv.dataset);
+console.log(testDiv.dataset.myData);
+console.log(testDiv.dataset.productName);
