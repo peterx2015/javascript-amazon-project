6afe6c96c82d81f0491a1e3716061c232844af26
peterx2015 <liyufeng2012@outlook.com>
2024-03-14 22:00:12 +0800

    在结帐页面，通过js生成html

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},
+{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}
+];
 
 //添加到购物车【product对象】
 export function addToCart(productId) {
